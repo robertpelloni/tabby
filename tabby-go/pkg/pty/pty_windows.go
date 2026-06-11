7f08e2d45fd405054037d5a62b3cff61928b7b4d
Robert Pelloni <pelloni.robert@gmail.com>
2026-06-11 12:51:55 -0400

    fix: use temp .bat script to set chcp 65001 before shell
    Creates a temporary .bat file with:
      @echo off
      chcp 65001 >nul
      <shell command>
    
    This sets the UTF-8 code page inside the ConPTY before the shell starts,
    avoiding all cmd.exe quoting issues with paths containing spaces.

--- tabby-go/pkg/pty/pty_windows.go
+++ tabby-go/pkg/pty/pty_windows.go
@@ -58,6 +58,17 @@ func (m *Manager) Spawn(params api.PTYSpawnParams) (*api.PTYSpawnResult, error) 
 		cmdLine += " " + arg
 	}
 
+	// Create a startup script that sets UTF-8 code page then runs the shell
+	// This avoids cmd.exe quoting issues with paths containing spaces
+	startupScript := "@echo off\r\nchcp 65001 >nul\r\n" + cmdLine
+	tmpFile, err := os.CreateTemp("", "tabby-startup-*.bat")
+	if err == nil {
+		tmpFile.WriteString(startupScript)
+		tmpFile.Close()
+		cmdLine = tmpFile.Name()
+		defer os.Remove(tmpFile.Name())
+	}
+
 	// Build environment with UTF-8 settings
 	env := os.Environ()
 	if params.Env != nil {
